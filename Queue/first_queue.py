a4b2e5db7e48d8e914657e02bb1ce08385d111bc
Tin Pham <72054441+tinpham5614@users.noreply.github.com>
2022-07-07 14:38:50 -0700

    Update first_queue.py

--- Queue/first_queue.py
+++ Queue/first_queue.py
@@ -1,11 +1,61 @@
-arr = []
+class LinkedListNode:
 
-arr.append(1)
-arr.append(2)
-arr.append(3)
+  def __init__(self, val):
+    self.val = val
+    self.next = None
 
-arr.pop(0)
+class Queue:
 
-print(arr)
+  def __init__(self):
+    self._head = None
+    self._tail = None
+    self._len = 0
 
-# output: [2,3]
+  def enqueue(self, val):
+    new_lln = LinkedListNode(val)
+    if not self._len:
+      self._len += 1
+      self._head = new_lln
+      self._tail = new_lln
+      return
+    self._len += 1
+    self._tail.next = new_lln
+    self._tail = new_lln
+
+  def dequeue(self):
+    if not self._len:
+      raise ValueError('Cannot dequeue from empty queue!')
+    self._len -= 1
+    val = self._head.val
+    self._head = self._head.next
+    return val
+
+  def __repr__(self):
+    if not self._len:
+      return 'Empty queue!'
+    vals = []
+    temp = self._head
+    while temp is not None:
+      vals.append(str(temp.val))
+      temp = temp.next
+    return ' -> '.join(vals)
+
+q = Queue()
+q.enqueue(5)
+q.enqueue(4)
+q.enqueue(1)
+print(q)
+q.dequeue()
+print(q)
+
+
+k = 3
+running_total = 0
+q = Queue()
+for user_input_num in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+  running_total += user_input_num
+  q.enqueue(user_input_num)
+  if q._len > k:
+    running_total -= q.dequeue()
+  print('Current Queue: ' + str(q))
+  print('Running Average: ' + running_total / q._len)
