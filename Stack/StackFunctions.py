6538db69adf51a360818384e6e71189d49858e2e
Tin Pham <72054441+tinpham5614@users.noreply.github.com>
2022-07-09 11:35:11 -0700

    Update StackFunctions.py

--- Stack/StackFunctions.py
+++ Stack/StackFunctions.py
@@ -1,13 +1,19 @@
 #Stack a bunch of stuff, LAST IN - FIRST OUT (LIFO)
 #two basic operations - PUSH and POP
 # push == add, pop == delete
-class MinStack:
-  def __init__(self):
+class Stack:
+  def __init__(self): # create an empty stack
     self._stack = []
-  def push(self, x):
+    
+  def push(self, x): # create adding function
     self._stack.append(x)
-  def pop(self):
+    
+  def pop(self): # create remove function
     return self._stack.pop()
-  def peek(self):
-    return self._stack.peek()
+  
+  def peek(self): # create checking the last item but do not remove that item
+    return self._stack.x[len(self._stack)-1]
+  
+  def size(self): # create the length function of the stack
+    return len(self._stack)
   
